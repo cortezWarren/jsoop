f09f6a65547e736be01b75e7365d6a878d736f25
cortezWarren <92365206+cortezWarren@users.noreply.github.com>
2022-10-01 12:56:03 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,4 +3,18 @@ class User {
         this.email = email;
         this.age = age;
     }
+
+    logIn() {
+        console.log(this.email, 'has logged in');
+        return this;
+    }
+    logOut (){
+        console.log(this.email, 'has logged out');
+        return this;
+
+    }
 }
+
+let uOne = new User('g@gmail.com',22);
+
+uOne.logIn().logOut();
